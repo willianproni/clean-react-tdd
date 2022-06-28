753b588b992ebc20966209f5eee16ba2c1c0c90e
willianproni <wifepro@hotmail.com>
2022-06-28 14:27:00 -0300

    test: eusure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,8 +1,10 @@
 import { RemoteAuthentication } from "./remote-authentication"
 import { HttpPostClientSpy } from "@/data/test/mock-http-client"
 import { mockAuthentication } from "@/domain/test/mock-authentication";
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
 
 import faker from 'faker'
+import { HttpStatusCode } from "@/data/protocols/http/http-response";
 
 
 type SutTypes = {
@@ -32,5 +34,14 @@ describe('RemoteAuthentication', () => {
         const authenticationParams = mockAuthentication() 
         await sut.auth(authenticationParams)
         expect(httpPostClientSpy.body).toEqual(authenticationParams)
+    })
+
+    test('Should throw InvalidCredentialsError if HttpPostClient returns 401', async () => {
+        const {sut, httpPostClientSpy} = makeSut()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.unathorized
+        }
+        const promise = sut.auth(mockAuthentication())
+        await expect(promise).rejects.toThrow(new InvalidCredentialsError())
     })
 })
